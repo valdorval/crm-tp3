3cdb396bdd6ca96e10297c66fbeca39df5e07eb0
val <vale.dorval@gmail.com>
2020-10-09 14:23:17 -0400

    boucle

--- custom-page/page-blog.php
+++ custom-page/page-blog.php
@@ -5,6 +5,19 @@
  *
  */
 get_header();
+?>
 
+<?php if (have_posts()) : ?>
+     <ul>
+          <?php while (have_posts()) : the_post(); ?>
+               <li><?php the_title(); ?></li>
+          <?php endwhile; ?>
+     </ul>
+<?php else : ?>
+     <h1>Aucun article trouvé</h1>
 
+<?php endif; ?>
+
+<?php
 get_footer();
+?>
