e3607951a5f94287249152bd65daca43999d207e
val <vale.dorval@gmail.com>
2020-10-09 17:39:00 -0400

    categories

---
+++ category-nouvelles.php
@@ -0,0 +1,6 @@
+<?php
+get_header();
+
+?>
+
+nouvelles
--- custom-page/page-blog.php
+++ custom-page/page-blog.php
@@ -6,17 +6,24 @@
  */
 get_header();
 ?>
+<?php
+$categories = get_categories();
+foreach ($categories as $categorie) {
+     echo '<div><a href="' . get_category_link($categorie->term_id) . '">' . $categorie->name . '</a></div>';
+}
+?>
 
-<?php if (have_posts()) : ?>
-     <ul>
-          <?php while (have_posts()) : the_post(); ?>
-               <li><?php the_title(); ?></li>
-          <?php endwhile; ?>
-     </ul>
-<?php else : ?>
-     <h1>Aucun article trouvé</h1>
+<?php // affiche les 6 articles les plus récents
+$wp_query = new WP_Query(array('posts_per_page' => 6));
 
-<?php endif; ?>
+if ($wp_query->have_posts()) :
+     while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
+          <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+          <h5><?php the_category(); ?></h5>
+          <p><?php the_excerpt('Suite'); ?></p>
+
+<?php endwhile;
+endif; ?>
 
 <?php
 get_footer();
