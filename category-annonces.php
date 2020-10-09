52d47d10a1ce46dd8cac0eb81843faf800335594
val <vale.dorval@gmail.com>
2020-10-09 17:47:59 -0400

    historique catégories

--- category-annonces.php
+++ category-annonces.php
@@ -3,4 +3,18 @@ get_header();
 
 ?>
 
-annonces
+<h1>Annonces</h1>
+
+<?php query_posts('cat=2');
+if (have_posts()) : ?>
+     <ul>
+          <?php while (have_posts()) : the_post(); ?>
+               <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+     </ul>
+
+<?php endwhile;
+     endif; ?>
+
+<?php
+get_footer();
+?>
--- category-evenements.php
+++ category-evenements.php
@@ -3,4 +3,19 @@ get_header();
 
 ?>
 
-evenement
+<h1>Évènements</h1>
+
+<?php query_posts('cat=3');
+
+if (have_posts()) : ?>
+     <ul>
+          <?php while (have_posts()) : the_post(); ?>
+               <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+     </ul>
+
+<?php endwhile;
+     endif; ?>
+
+<?php
+get_footer();
+?>
