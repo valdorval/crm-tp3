2a6a2048a96c6c8ca82544db070d4cfd18ae16ee
SAFA <sammoudasaffa@gmail.com>
2020-10-16 15:06:38 -0400

    fonctionnalités

--- template-parts/accueil.php
+++ template-parts/accueil.php
@@ -1,74 +1,73 @@
 <main id="main">
-<section class="main-bloc">
-    <div class="cardcontainer">
-    <div class="card">
-        <div class="card--imgBx">
-            <img src="<?php echo get_template_directory_uri(); ?>/img/contact.png" alt="title1">
-            <div class="card--contentBx">
-              <div class="card--contentBx--title">HERE</div>
+    <section class="main-bloc">
+        <div class="cardcontainer">
+            <div class="card">
+                <div class="card--imgBx">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/contact.png" alt="title1">
+                    <div class="card--contentBx">
+                        <div class="card--contentBx--title"> Garder des traces à vos interactions</div>
+                      <p>  En possédant de nombreux clients, il est impossible de se souvenir de tous, de se rappeler précisément de leur activité, de leur caractéristiques, des derniers échanges que l’on a eu avec eux .<br>Grâce à CosTOMers, vous pouvez vous rafraîchir la mémoire et tout connaître de n’importe quel client en un clin d’œil.</p>
+                    </div>
 
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
             </div>
 
-        </div>
-    </div>
+            <div class="card">
+                <div class="card--imgBx">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/reviewer.png" alt="title1">
+                    <div class="card--contentBx">
+                        <div class="card--contentBx--title">Vos historiques en un clic </div>
+                        <p>Chaque jour, de nombreuses opportunités se présentent à vous .<br>Or, il peut être compliqué de garder une trace de chacun de ces prospects, surtout s’ils restent « en sommeil » pendant des mois (voire des années). CosTOMers permet justement de simplifier cette tâche et de se souvenir de tout à votre place.</p>
+                   </div>
 
-    <div class="card">
-        <div class="card--imgBx">
-            <img src="<?php echo get_template_directory_uri(); ?>/img/reviewer.png" alt="title1">
-            <div class="card--contentBx">
-                <div class="card--contentBx--title">HERE</div>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
             </div>
+            <div class="card">
+                <div class="card--imgBx">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/profile3.png" alt="title1">
+                    <div class="card--contentBx">
+                        <div class="card--contentBx--title">Des relations catégorisées </div>
 
-        </div>
-    </div>
-    <div class="card">
-        <div class="card--imgBx">
-            <img src="<?php echo get_template_directory_uri(); ?>/img/contact.png" alt="title1">
-            <div class="card--contentBx">
-              <div class="card--contentBx--title">HERE</div>
+                        <p>Organiser vos clients selon le stade auquel ils en sont dans le processus va optimiser votre cycle de vente.<br>
+                        Réussir à comprendre  l’importance de frapper à la bonne étape du processus décisionnel de l’acheteur et de le rendre aussi simplifié que possible pour le client.</p>
+                    </div>
 
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
             </div>
+            <div class="card">
+                <div class="card--imgBx">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/profile5.png" alt="title1">
+                    <div class="card--contentBx">
+                        <div class="card--contentBx--title">Filtrer les suivis pour vos clients</div>
 
-        </div>
-    </div><div class="card">
-        <div class="card--imgBx">
-            <img src="<?php echo get_template_directory_uri(); ?>/img/contact.png" alt="title1">
-            <div class="card--contentBx">
-              <div class="card--contentBx--title">HERE</div>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    </div>
 
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
             </div>
+            <div class="card">
+                <div class="card--imgBx">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/simple.png" alt="title1">
+                    <div class="card--contentBx">
+                        <div class="card--contentBx--title">HERE</div>
 
-        </div>
-    </div><div class="card">
-        <div class="card--imgBx">
-            <img src="<?php echo get_template_directory_uri(); ?>/img/contact.png" alt="title1">
-            <div class="card--contentBx">
-              <div class="card--contentBx--title">HERE</div>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    </div>
 
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
             </div>
+            <div class="card">
+                <div class="card--imgBx">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/info.png" alt="title1">
+                    <div class="card--contentBx">
+                        <div class="card--contentBx--title">HERE</div>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    </div>
 
-        </div>
-    </div>
-    <div class="card">
-        <div class="card--imgBx">
-            <img src="<?php echo get_template_directory_uri(); ?>/img/info.png" alt="title1">
-            <div class="card--contentBx">
-                <div class="card--contentBx--title">HERE</div>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
             </div>
-
         </div>
-    </div>
-   </div>
-</section>
+    </section>
